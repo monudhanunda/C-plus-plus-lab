d31fec6c77d985fbc199454b8acb4e66d70dac98
Ravinder191952 <60093908+monudhanunda@users.noreply.github.com>
2021-03-21 21:08:47 +0530

    Add files via upload

---
+++ Inline function r.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+inline double power(double m,int n=2)
+{
+	double t;
+	t=pow(m,n);
+	return t;
+}
+int main()
+{
+	double num,ans;
+	int p;
+	cout<<"Enter the number: \n";
+	cin>>num;
+
+	cout<<"if you dont want to enter power press 0 : ";
+	cin>>p;
+
+	if(p==0)
+	{
+		ans=power(num);
+		cout<<"\nSquare of "<<num<<" is "<<ans<<endl;
+	}
+	else
+	{
+		cout<<"\n enter the power:";
+		cin>>p;
+	
+		ans=power(num,p);
+		cout<<"\nFinal answer is : "<<ans<<endl;
+	}
+}
